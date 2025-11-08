3a93f98b87f6c4599a3275b2ffdaaf3d4c9d350d
Varagan77 <johanneswillemkotze@gmail.com>
2025-11-08 21:17:16 +0200

    Init pywhatkit
    This will be used to send messages. Logic to come

---
+++ Project/bin/whatsapp_script.py
@@ -0,0 +1,8 @@
+import pywhatkit as pwk
+
+try:
+    pwk.sendwhatmsg_instantly("+--------------", "Hello World", wait_time=10, tab_close=True)
+    print("Message Sent!")
+except Exception as e:
+    print("Error in sending the message:", e)
+
